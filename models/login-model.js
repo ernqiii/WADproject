e5691368ef787316618843eaeab6c3dc82b0964d
nxpour2025-sketch <nx.pour.2025@smu.edu.sg>
2026-03-17 16:41:12 +0800

    Update login-model.js

--- models/login-model.js
+++ models/login-model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  email: {type: String, required: true},
-  gender: {type: String, required: true},
-  bio: {type: String, required: true}
+  username: {type: String, required: true, trim: true, unique: true},
+  password: {type: String, required: true, trim: true},
+  fullname: {type: String, required: true, trim: true},
+  phone: {type: String, required: true, trim: true},
+  email: {type: String, required: true, trim: true},
+  gender: {type: String, required: true, trim: true},
+  bio: {type: String, required: true, trim: true}
 });
 
 module.exports = mongoose.model("User", userSchema);
