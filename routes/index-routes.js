a21c9d865de92aa1d3a61b667eb1327f9125d1e2
nxpour2025-sketch <nx.pour.2025@smu.edu.sg>
2026-03-30 13:13:13 +0800

    Update index-routes.js

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,4 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
 const indexController = require("../controllers/index-controller");
-const router = require("./login-routes");
+const loginController = require("../controllers/login-controller");
 
 router.get("/", indexController.showIndex);
+
+router.get("/login-form", loginController.displayLoginForm);
+router.post("/login-form", loginController.handleLogin);
+
+router.get("/signup-form", loginController.displaySignupForm);
+router.post("/signup-form", loginController.handleSignup);
+
+router.get("/logout", loginController.handleLogout);
+
+module.exports = router;
