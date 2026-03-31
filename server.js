bf1e939ed3b4736c30d593cde42861d4db65d085
chloeang88 <chloe.a.2025@computing.smu.edu.sg>
2026-03-31 17:53:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use("/",express.static(path.join(__dirname,"/public")));
 
-server.use(express.static(path.join(__dirname, "public")));
+
 
 const secret = process.env.SECRET;
 server.use(session({
